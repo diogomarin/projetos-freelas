0a923abf279eff52a83dfa271f8062452114757f
diogomarin <70486076+diogomarin@users.noreply.github.com>
2023-11-02 22:35:01 -0300

    att name folder

---
+++ automacao-planilhas-erp/setup.py
@@ -0,0 +1,23 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Arquivo Python que você deseja compilar
+target_file = 'layout.py'
+
+# Configurações de compilação
+build_options = {
+    'packages': ['PySimpleGUI', 'pandas', 'openpyxl', 'shutil'],
+    'includes': ['tkinter', 'tkinter.ttk', 'tkinter.simpledialog'],
+}
+
+# Criação do executável
+executables = [Executable(target_file, base='Win32GUI', target_name ='AppContabilMultiplos.exe')]
+
+# Configuração de setup
+setup(
+    name='AppContabilMultiplos',
+    version='1.0',
+    description='Descrição do seu programa',
+    options={'build_exe': build_options},
+    executables=executables
+)
